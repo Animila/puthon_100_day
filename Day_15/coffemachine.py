6cf687064d724b5c33a3639141eb01151f1f963d
Animila <hristoforovila5@gmail.com>
2022-02-15 20:16:42 +0300

    Learning Day 15 (v_2)

--- Day_15/coffemachine.py
+++ Day_15/coffemachine.py
@@ -36,54 +36,61 @@ resources = {
     "coffee": 100,
 }
 
-money = {
-    'penny': 0.01,
-    'nickel': 0.05,
-    'dime': 0.10,
-    'quarter': 0.25,
 
-}
-
-
-def money_check():
-    one = money['quarter'] * int(input('Сколько центов?: '))
-    two = money['dime'] * int(input('Сколько динариев?: '))
-    three = money['nickel'] * int(input('Сколько фунты?: '))
-    four = money['penny'] * int(input('Сколько пенни?: '))
-    sum = round(one + two + three + four, 2)
-    return sum
+def resource(order_ingredient):
+    '''Возвращает True или False если не хватает ресурсов'''
+    no_need = True
+    for item in order_ingredient:
+        if order_ingredient[item] >= resources[item]:
+            print('Простите, но нет воды')
+            no_need = False
+    return no_need
 
 
-def drink_give(user):
-    drink_user = MENU[user]
-    structure = drink_user['ingredients']
-    if structure['water'] >= resources['water']:
-        resources['water'] -= structure['water']
-
-    if structure['coffee'] >= resources['coffee']:
-        resources['coffee'] -= structure['coffee']
-
-    if user != 'expresso':
-        if structure['milk'] >= resources['milk']:
-            resources['milk'] -= structure['milk']
+def process_coins():
+    '''Возврат общей суммы монет'''
+    print('Введите моенты')
+    total = int(input('Сколько четвертаков?: ')) * 0.25
+    total += int(input('Сколько цент?: ')) * 0.1
+    total += int(input('Сколько пятаков?: ')) * 0.05
+    total += int(input('Сколько пенни?: ')) * 0.01
+    return total
 
 
-def reporting():
-    print(f'Вода: {resources["water"]}ml')
-    print(f'Молоко: {resources["milk"]}ml')
-    print(f'Кофе: {resources["coffee"]}ml')
+def successful(money_received, drink_cost):
+    '''Возвращает True когда платеж принят, или False если не прошло внимания'''
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Получите ${change} сдачи")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Прошу прощения, платеж не принят, денег мало")
+        return False
+
+def make_coffee(drink_name, order_ingredient):
+    '''Вычитает необходимые ингредиенты'''
+    for item in order_ingredient:
+        resources[item] -= order_ingredient[item]
+    print(f'Получите свой {drink_name}')
 
 
-def CoffeMachine():
-    exit = False
-    while not exit:
-        user = input('Что бы вы хотели? (latte/expresso/cappuccino): ')
-        if user == 'report':
-            reporting()
-        elif user == 'exit':
-            exit = True
-        else:
-            drink_give(user)
+profit = 0
+exit = False
 
-
-CoffeMachine()
+while not exit:
+    choose = input('Что бы вы хотели? (espresso/latte/cappuccino): ')
+    if choose == 'off':
+        exit = True
+    elif choose == 'report':
+        print(f'Вода: {resources["water"]}ml')
+        print(f'Молоко: {resources["milk"]}ml')
+        print(f'Кофе: {resources["coffee"]}ml')
+        print(f'Монеты: ${profit}')
+    else:
+        drink = MENU[choose]
+        if resource(drink["ingredients"]):
+            payment = process_coins()
+            if successful(payment, drink['cost']):
+                make_coffee(choose, drink['ingredients'])
